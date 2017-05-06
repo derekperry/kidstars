f89ca7185831132f3593d52d9032685eeacc8a36
derek <derek@derekperry.com>
2017-05-06 17:20:22 -0400

    begin development of front end

--- kidstars/root/views.py
+++ kidstars/root/views.py
@@ -3,10 +3,10 @@ from __future__ import absolute_import
 from flask import Blueprint, render_template
 from flask_security import login_required, current_user
 
-from app import app
+root = Blueprint('root', __name__, static_folder='static', template_folder='templates', url_prefix='')
 
 
-@app.route('/')
+@root.route('/')
 def hello_world():
     if not current_user.is_authenticated:
         return 'Not logged in  - Hello World!'
@@ -14,15 +14,15 @@ def hello_world():
         return 'Hello World!'
 
 
-@app.route('/test')
+@root.route('/test')
 @login_required
 def hello_world1():
     return 'Hello World!'
 
-@app.route('/kidstars')
+@root.route('/kidstars')
 def login_page():
     return render_template('login.html')
 
-@app.route('/dptest')
+@root.route('/dptest')
 def dptest():
     return 'cool!'
