d1e3c830200d7f41934c21f9562ac6a97793595c
derek <derek@derekperry.com>
2017-05-10 20:27:08 -0400

    Child entity form and add view

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -6,11 +6,32 @@ from flask_migrate import Migrate, MigrateCommand
 from app import app, db
 from kidstars.models import User, Role, Connection
 
+import urllib
+
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.parse.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print(line)
 
 if __name__ == '__main__':
     manager.run()
